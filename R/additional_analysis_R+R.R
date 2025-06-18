85e2ffe1ea35465fe1a20791ccdd39133fbee959
gidonc <gidon.cohen@durham.ac.uk>
2025-06-18 18:15:43 +0100

    Add some additional analyses

--- R/additional_analysis_R+R.R
+++ R/additional_analysis_R+R.R
@@ -21,3 +21,29 @@ f4 <-lm_robust(manualresponse_exists_inc_post_count_nonresponse~ autoresponse_ex
 huxtable::huxreg(f1, f1a, f2, f3, f4, coefs = c("(Intercept)",
                                            "autoresponse_exists",
                                            "autoresponse_prop"))
+
+##----Further-Analysis-and-Tables-Not-in-Paper-----
+
+##----response-rate-descriptives-party-----
+main %>% 
+  mutate(party=case_when(
+    party == "lab" ~ "Labour",
+    party == "con" ~ "Conservative",
+    TRUE ~ "Other"
+  )) %>%
+  group_by(party) %>%
+  summarise(
+    substantive_response = scales::percent(mean(manualresponse_exists_inc_post_count_nonresponse), accuracy = .1)
+  ) %>%
+  rename(
+    " " = party,
+    "Response rate"=substantive_response
+  ) %>%
+  huxtable()%>% 
+  set_bold(row = 1, col = everywhere) %>% 
+  set_bottom_border(row = 1, col = everywhere,  brdr(.4, "solid", "black")) %>% 
+  set_right_border(everywhere, 1,  brdr(.4, "solid", "black")) %>%
+  # set_background_color(evens, everywhere, "grey95") %>%
+  set_caption("Substantive response rate by MP party.")
+
+
